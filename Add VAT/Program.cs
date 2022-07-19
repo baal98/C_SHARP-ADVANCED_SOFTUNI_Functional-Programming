3a44cf0422a9e8934026d684f5224b0062e796e8
Dimo Todorov <baal98@gmail.com>
2022-07-19 22:06:48 +0300

    Add files via upload

---
+++ Add VAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).Select(x => x * 1.2);
+
+            foreach (var input in inputs)
+            {
+                Console.WriteLine($"{input:F2}");
+            }
+        }
+    }
+}
