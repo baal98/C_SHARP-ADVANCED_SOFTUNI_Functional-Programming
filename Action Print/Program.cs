fd87cf6c413ef135e59a9736b7263560c654d0e7
Dimo Todorov <baal98@gmail.com>
2022-07-19 22:06:21 +0300

    Add files via upload

---
+++ Action Print/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printInputs = input => Console.WriteLine(input);
+
+            foreach (var input in inputs)
+            {
+                printInputs(input);
+            }
+        }
+    }
+}
