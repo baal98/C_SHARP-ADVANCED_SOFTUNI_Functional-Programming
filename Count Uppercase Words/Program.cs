d99026fe4ef4ba438d5cb1b8bf74cb26811346d1
Dimo Todorov <baal98@gmail.com>
2022-07-20 07:09:56 +0300

    Add files via upload

---
+++ Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(x => char.IsUpper(x[0]));
+
+            Console.WriteLine(String.Join("\n", inputs));
+        }
+    }
+}
