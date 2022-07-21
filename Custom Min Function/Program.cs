2debd0a0067618f9d20eeae4423aa141dc71d124
Dimo Todorov <baal98@gmail.com>
2022-07-21 18:02:55 +0300

    Add files via upload

---
+++ Custom Min Function/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            Func<double[], double> func = MinValue;
+            var value = func(inputs);
+            Console.WriteLine(value);
+
+        }
+
+        private static double MinValue(double[] inputs)
+        {
+            double min = double.MaxValue;
+            foreach (var input in inputs)
+            {
+                if (input < min)
+                {
+                    min = input;
+                }
+            }
+            return min;
+        }
+    }
+}
