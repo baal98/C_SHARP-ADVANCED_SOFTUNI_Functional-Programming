9cc88bff20f5633a5c493f7c283f60c275e000c8
Dimo Todorov <baal98@gmail.com>
2022-07-22 06:57:19 +0300

    Add files via upload

---
+++ Knights of Honor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split();
+
+            Action<string> action = input => PrintName(input);
+
+            foreach (var input in inputs)
+            {
+                action(input);
+            }
+        }
+        private static void PrintName(string input)
+        {
+            Console.WriteLine($"Sir {input}");
+        }
+    }
+}
