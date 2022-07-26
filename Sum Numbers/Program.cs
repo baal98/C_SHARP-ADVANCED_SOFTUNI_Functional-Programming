b8c17cc34400ad06bcea04e659ef812972973d12
Dimo Todorov <baal98@gmail.com>
2022-07-26 06:09:48 +0300

    Add files via upload

---
+++ Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse);
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
